117b508543e08d83d9e697cf2413600549a9e5f1
zhoum@huizuche.com <zhoum@huizuche.com>
2017-05-09 14:23:10 +0800

    new

---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>weexproject</h1>
+    <p>A vue project.</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  };
+</script>
